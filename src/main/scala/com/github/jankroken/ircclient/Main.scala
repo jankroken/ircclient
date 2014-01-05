79aae3e2f7cc21592957fba380228d1109ea916b
Jan Kroken <jan.kroken@gmail.com>
2014-01-05 21:54:50 +0100

    yay! the gui isnt immutable now

--- src/main/scala/com/github/jankroken/ircclient/Main.scala
+++ src/main/scala/com/github/jankroken/ircclient/Main.scala
@@ -10,6 +10,7 @@ import scalafx.scene.layout._
 import scalafx.scene.Scene
 import scalafx.stage.Screen
 import scalafx.scene.paint.Color
+import scalafx.collections.ObservableBuffer
 
 
 object Main extends JFXApp {
@@ -79,15 +80,24 @@ object Main extends JFXApp {
     items.addAll(nickPanel,channelPanel)
   }
 
+  val ob = ObservableBuffer[HBox]()
+
   val channel = new ScrollPane {
     fitToWidth = true
     fitToHeight = true
+    val panel = new GridPane {
+
+    }
+    content = panel
+/*
+
     content = new VBox {
       fitToWidth = true
-      content = lines
+//      content = lines
+      content = ob
     }
+    */
   }
-
 
   stage = new PrimaryStage {
     title = "IRC Client"
@@ -105,96 +115,9 @@ object Main extends JFXApp {
       }
     }
   }
+
+  List.range(0,999).foreach{n => {
+    channel.panel.addRow(n,lines(n))
+  }}
+
 }
-
-
-/*
-
-/** The main ScalaFX Ensemble application object. */
-object Ensemble extends JFXApp {
-
-  //
-  // Example selection tree
-  //
-  var centerPane = PageDisplayer.choosePage("dashBoard")
-  val rootTreeItem = new TreeItem[String]("ScalaFX Ensemble") {
-    expanded = true
-    children = EnsembleTree.create().getTree
-  }
-
-  val screen = Screen.primary
-  val controlsView = new TreeView[String]() {
-    minWidth = 200
-    maxWidth = 200
-    editable = true
-    root = rootTreeItem
-    id = "page-tree"
-  }
-  controlsView.selectionModel().selectionMode = SelectionMode.SINGLE
-  controlsView.selectionModel().selectedItem.onChange {
-    (_, _, newItem) => {
-      val pageCode = (newItem.isLeaf, Option(newItem.getParent)) match {
-        case (true, Some(parent)) => parent.getValue.toLowerCase + " > " + newItem.getValue
-        case (false, Some(_))     => "dashBoard - " + newItem.getValue
-        case (_, _)               => "dashBoard"
-      }
-      centerPane = PageDisplayer.choosePage(pageCode)
-      splitPane.items.remove(1)
-      splitPane.items.add(1, centerPane)
-    }
-  }
-
-  val scrollPane = new ScrollPane {
-    minWidth = 200
-    maxWidth = 200
-    fitToWidth = true
-    fitToHeight = true
-    id = "page-tree"
-    content = controlsView
-  }
-  val splitPane = new SplitPane {
-    dividerPositions = 0
-    id = "page-splitpane"
-    items.addAll(scrollPane, centerPane)
-  }
-
-  //
-  // Layout the main stage
-  //
-  stage = new PrimaryStage {
-    title = "ScalaFX Ensemble"
-    scene = new Scene(1020, 700) {
-      root = new BorderPane {
-        top = new VBox {
-          vgrow = Priority.ALWAYS
-          hgrow = Priority.ALWAYS
-          content = new ToolBar {
-            prefHeight = 76
-            maxHeight = 76
-            id = "mainToolBar"
-            content = List(
-              new ImageView {
-                image = new Image(
-                  this.getClass.getResourceAsStream("/scalafx/ensemble/images/logo.png"))
-                margin = Insets(0, 0, 0, 10)
-              },
-              new Region {
-                minWidth = 300
-              },
-              new Button {
-                minWidth = 120
-                minHeight = 66
-                id = "newButton"
-              })
-          }
-        }
-        center = new BorderPane {
-          center = splitPane
-        }
-        styleClass += "application"
-      }
-    }
-    scene().stylesheets += this.getClass.getResource("/scalafx/ensemble/css/ensemble.css").toExternalForm
-  }
-}
-*/
